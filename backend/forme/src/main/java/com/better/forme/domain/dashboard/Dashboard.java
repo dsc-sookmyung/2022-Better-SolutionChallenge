dd85087ec7d8ea2d3adbcaa81778b7125abe8df1
Eundongdong <51512016+Eundongdong@users.noreply.github.com>
2022-03-01 20:51:42 +0900

    build(application, build.gradle), test(Dashboard file) : connect mysql and handle some test codes
    GCP에 내장되어있는 SQL과 연결하기 위해 application.properties(기존 파일명에 오류가 있어 수정했습니다.), build.gradle(톰캣 관련 설정 추가하였습니다.)에 변경사항이 있습니다. 또 잘 작동하는지 확인하기 위한 Dashboard entity, repository 추가되어있습니다. + mysql연결 성공하여 커밋함

---
+++ backend/forme/src/main/java/com/better/forme/domain/dashboard/Dashboard.java
@@ -0,0 +1,24 @@
+package com.better.forme.domain.dashboard;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@NoArgsConstructor
+@Entity
+@Data
+public class Dashboard {
+    @Id
+    private Long id;
+
+    private String user;
+
+    private String password;
+
+    private String title;
+
+
+}
