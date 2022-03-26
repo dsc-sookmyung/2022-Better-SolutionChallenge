96011f32ae3a55eee314dc71060e98d0c1699af5
BoKyung <leebk1124@gmail.com>
2022-03-26 16:31:03 +0900

    Delete LoginRecipe/RegisterLoginPage2 directory

--- LoginRecipe/RegisterLoginPage2/app/src/main/java/com/example/registerloginpage/model/ForMe.kt
+++
@@ -1,7 +0,0 @@
-package com.example.registerloginpage.model
-
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
-
-class ForMe (@StringRes val stringResourceId: Int, @DrawableRes val imageResourceId: Int) {
-}
